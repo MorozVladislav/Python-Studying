26fd3b8322454a97d7d6f25911bdcae430e97da4
Dmitriy Moroz <d_moroz@wargaming.net>
2017-10-28 01:23:38 +0300

    task_2 added

---
+++ tasks/task_2.py
@@ -0,0 +1,26 @@
+"""
+TASK: Create 'Fake' class to run code below without errors.
+"""
+
+import random
+import string
+
+
+# INSERT YOUR CODE HERE
+
+
+def generate_string(length=10):
+    return ''.join(random.choice(string.ascii_letters) for _ in range(length))
+
+
+if __name__ == '__main__':
+    fake = Fake()
+    fake.non_existing_method('argument')
+    fake.attribute
+    fake[42]
+    fake['non existing key']
+    fake[generate_string()]
+    fake2 = fake.blablabla()
+    fake2.some_name()
+    getattr(fake2, generate_string())()
+    fake2.whatever.again_whatever().and_again['foo']['bar'][1]
