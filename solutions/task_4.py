3835dffa18a1adf493000d179d635396ceaa5843
Vladislav <mvs.vladislav@gmail.com>
2017-12-21 23:39:03 +0300

    First version. Regular expressions have been used

--- solutions/task_4.py
+++ solutions/task_4.py
@@ -2,11 +2,55 @@
 """
 TASK: Implement classes "Temperature", "Person" using Python descriptor protocols to execute unittests without errors.
 """
+import re
 import unittest
 from datetime import datetime
 
 
-# INSERT YOUR CODE HERE
+class Temperature(object):
+
+    def __init__(self, value=0):
+        self.fahrenheit = value
+
+    def get_celsius(self):
+        return (self.fahrenheit - 32)/1.8
+
+    def set_celsius(self, value):
+        self.fahrenheit = value*1.8 + 32
+
+    celsius = property(get_celsius, set_celsius)
+
+
+class Descriptor(object):
+
+    def __init__(self, attr_name, attr_type):
+        self.attr_name = attr_name
+        self.attr_type = attr_type
+
+    def __get__(self, instance, owner):
+        return instance.__dict__[self.attr_name]
+
+    def __set__(self, instance, attr_value):
+        if self.attr_type != type(attr_value):
+            raise TypeError
+        if self.attr_name == 'phone':
+            pattern = r"[0-9]{3} [0-9]{2} [0-9]{7}$"
+            if re.match(pattern, attr_value):
+                instance.__dict__.__setitem__(self.attr_name, "+{} ({}) {}-{}-{}".format(attr_value[:3],
+                                                                                         attr_value[4:6],
+                                                                                         attr_value[7:10],
+                                                                                         attr_value[10:12],
+                                                                                         attr_value[12:]))
+            else:
+                raise ValueError
+        else:
+            instance.__dict__.__setitem__(self.attr_name, attr_value)
+
+
+class Person(object):
+    birthday = Descriptor('birthday', datetime)
+    name = Descriptor('name', str)
+    phone = Descriptor('phone', str)
 
 
 class TestTask4(unittest.TestCase):
