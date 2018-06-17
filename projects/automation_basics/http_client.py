d3629d4bad2d17655bedba20ba3792e30f6508f8
Vladislav <mvs.vladislav@gmail.com>
2018-06-17 15:30:28 +0300

    Another portion of improvments for HTTP client

--- projects/automation_basics/http_client.py
+++ projects/automation_basics/http_client.py
@@ -1,6 +1,7 @@
 import logging
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as XmlEtree
 from contextlib import contextmanager
+from urllib.parse import urljoin
 
 import requests
 
@@ -9,13 +10,13 @@ logger = logging.getLogger(__name__)
 
 class HttpClient(object):
 
-    def __init__(self, host=None, base_url=None, headers=None, cookies=None, timeout=None, single_session=False):
+    GET, POST, PUT, DELETE, HEAD, OPTIONS = 'GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS'
+
+    def __init__(self, host='', base_url='', single_session=False, **kwargs):
         self.host = host
         self.base_url = base_url
-        self.headers = headers
-        self.cookies = cookies
-        self.timeout = timeout
         self.single_session = single_session
+        self.kwargs = kwargs
         self._session = None
 
     @property
@@ -33,46 +34,46 @@ class HttpClient(object):
         if not self.single_session:
             self.session.close()
 
-    def make_request(self, url, method='GET', return_json=False, return_tree=False, expected_response_code=None,
-                     **kwargs):
+    def request(self, method, url, return_json=False, xml_tree=False, expected_rcode=None, **kwargs):
+        kwargs = self.kwargs.update(kwargs)
         with self.session_ctx() as s:
-            logger.debug('Making {} request, URL: {}, parameters: {}'.format(method, url, kwargs))
-            resp = s.request(method, ''.join(filter(None, (self.host, self.base_url, url))), headers=self.headers,
-                             cookies=self.cookies, timeout=self.timeout, **kwargs)
-            logger.debug('Response received in {}.{} sec. Status code {}'.format(resp.elapsed.seconds,
-                                                                                 resp.elapsed.microseconds,
-                                                                                 resp.status_code))
+            logger.debug('Making {} request, URL: {}, parameters: {}, headers: {}, payload: {}'.format(method, url,
+                                                                                                       kwargs['params'],
+                                                                                                       kwargs['headers'],
+                                                                                                       kwargs['data']))
+            resp = s.request(method, urljoin(self.host, self.base_url, url), kwargs)
+            logger.debug('Request executed in {} sec. Response code {}'.format(resp.elapsed.strftime, resp.status_code))
+            logger.debug(resp.text)
 
-            if expected_response_code is not None:
-                if resp.status_code != expected_response_code:
-                    logger.exception('Response code is {} but {} was expected'.format(resp.status_code,
-                                                                                      expected_response_code))
+            if expected_rcode is not None:
+                if resp.status_code != expected_rcode:
+                    logger.exception('Response code is {} but {} was expected'.format(resp.status_code, expected_rcode))
                     raise Exception('Unexpected response code {}'.format(resp.status_code))
 
             if return_json:
                 return resp.json()
-            if return_tree:
-                return ET.parse(resp.text)
+            if xml_tree:
+                return XmlEtree.parse(resp.text)
 
             return resp
 
-    def make_get_request(self, url, **kwargs):
-        return self.make_request(url, method='GET', **kwargs)
+    def get(self, url, **kwargs):
+        return self.request(self.GET, url, **kwargs)
 
-    def make_post_request(self, url, **kwargs):
-        return self.make_request(url, method='POST', **kwargs)
+    def post(self, url, **kwargs):
+        return self.request(self.POST, url, **kwargs)
 
-    def make_put_request(self, url, **kwargs):
-        return self.make_request(url, method='PUT', **kwargs)
+    def put(self, url, **kwargs):
+        return self.request(self.PUT, url, **kwargs)
 
-    def make_delete_request(self, url, **kwargs):
-        return self.make_request(url, method='DELETE', **kwargs)
+    def delete(self, url, **kwargs):
+        return self.request(self.DELETE, url, **kwargs)
 
-    def make_head_request(self, url, **kwargs):
-        return self.make_request(url, method='HEAD', **kwargs)
+    def head(self, url, **kwargs):
+        return self.request(self.HEAD, url, **kwargs)
 
-    def make_options_request(self, url, **kwargs):
-        return self.make_request(url, method='OPTIONS', **kwargs)
+    def options(self, url, **kwargs):
+        return self.request(self.OPTIONS, url, **kwargs)
 
     def close(self):
         if self._session is not None:
