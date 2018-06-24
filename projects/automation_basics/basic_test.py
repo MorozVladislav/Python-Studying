5b1ad57efdb153d8935ad93e80fac9e15c7fd86f
Vladislav <mvs.vladislav@gmail.com>
2018-06-25 00:02:08 +0300

    Creation of multiple tokens was added

--- projects/automation_basics/basic_test.py
+++ projects/automation_basics/basic_test.py
@@ -1,12 +1,13 @@
 import json
 import os
+import random
+import string
 
 from projects.automation_basics.http_client import HttpClient
 
 HOST = 'https://api.github.com/'
-USER = os.environ['TEST_USER']
-PASSWORD = os.environ['TEST_PASSWORD']
-TOKEN = os.environ['TEST_TOKEN']
+AUTH = os.environ['AUTH'].split(':')
+TOKEN = None
 
 
 class TestClass(object):
@@ -17,13 +18,13 @@ class TestClass(object):
         assert resp.status_code == 200
 
     def test_2(self):
-        client = HttpClient(host=HOST, user=USER, password=PASSWORD)
+        client = HttpClient(host=HOST, user=AUTH[0], password=AUTH[1])
         resp = client.get('user')
         assert resp.status_code == 200
         assert int(resp.headers['X-RateLimit-Limit']) == 5000
 
     def test_3(self):
-        client = HttpClient(host=HOST, user=USER, password=PASSWORD)
+        client = HttpClient(host=HOST, user=AUTH[0], password=AUTH[1])
         params = {'type': 'owner'}
         resp = client.get('user/repos', params=params)
         d = json.loads(resp.text)
@@ -31,8 +32,15 @@ class TestClass(object):
         assert d[0]['has_projects'] is True
 
     def test_4(self):
-        client = HttpClient(host=HOST, token=TOKEN)
+        client = HttpClient(host=HOST, user=AUTH[0], password=AUTH[1])
+        fingerprint = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
         params = {'type': 'owner'}
+        payload = {'scopes': ['repo', 'user', 'delete_repo'], 'fingerprint': fingerprint, 'note': 'tests'}
+        resp = client.post('authorizations', json=payload)
+        assert resp.status_code == 201
+        global TOKEN
+        TOKEN = json.loads(resp.text)['token']
+        client = HttpClient(host=HOST, token=TOKEN)
         resp = client.get('user/repos', params=params)
         d = json.loads(resp.text)
         assert resp.status_code == 200
