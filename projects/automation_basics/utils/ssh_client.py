b2bd503f89a48d4136489167d226d11a90d7e622
Vladislav <mvs.vladislav@gmail.com>
2018-07-19 00:06:48 +0300

    First version of SSH client

---
+++ projects/automation_basics/utils/ssh_client.py
@@ -0,0 +1,80 @@
+import paramiko
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+class SSHClient(object):
+
+    def __init__(self, host=None, username=None, password=None, allow_unknown_hosts=False, environment=None):
+        self.host = host
+        self.username = username
+        self.password = password
+        self.allow_unknown_hosts = allow_unknown_hosts
+        self.environment = environment
+        self.host_keys = paramiko.HostKeys()
+        self.client = paramiko.SSHClient()
+        self.host_keys = self.client.get_host_keys()
+        self._shell = None
+
+    def __del__(self):
+        self.client.close()
+        logger.info('SSH client was closed')
+
+    @property
+    def shell(self):
+        if self.shell is None:
+            message = 'Shell was not opened'
+            logger.error(message)
+            raise ShellNotOpenedException(message)
+        return self._shell
+
+    def add_system_known_hosts(self, path):
+        self.client.load_system_host_keys(path)
+
+    def clear_host_keys(self):
+        self.host_keys.clear()
+
+    def connect(self, *args, **kwargs):
+        try:
+            self.client.connect(self.host, username=self.username, password=self.password, *args, **kwargs)
+        except paramiko.SSHException:
+            message = 'Failed to connect {}'.format(self.host)
+            logger.error(message)
+            raise ConnectionFailedException(message)
+
+    def execute(self, command, *args, **kwargs):
+        if self.allow_unknown_hosts:
+            self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        return self.client.exec_command(command, environment=self.environment, *args, **kwargs)
+
+    def open_shell(self):
+        self._shell = self.client.invoke_shell()
+        logger.info('New shell was opened')
+
+    def execute_in_shell(self, command):
+        self.shell.send(command + '\n')
+        result = self.shell.recv(1024)
+        while self.shell.recv_ready():
+            result += self.shell.recv(1024)
+        str_result = str(result)
+        str_result.replace('\r', '')
+        return str_result
+
+    def close_shell(self):
+        self.shell.close()
+        self._shell = None
+        logger.info('Current shell was closed')
+
+
+class SSHClientException(Exception):
+    pass
+
+
+class ShellNotOpenedException(SSHClientException):
+    pass
+
+
+class ConnectionFailedException(SSHClientException):
+    pass
